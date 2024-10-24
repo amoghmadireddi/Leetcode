43822e61da6701f62c28bd6a02bc21c79bf3ca99
Amogh Madireddi <amoghmm@umich.edu>
2024-10-24 08:26:27 -0400

    Time: 0 ms (100%), Space: 9.4 MB (86.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,8 +10,11 @@ public:
         int temp;
         // we will use a to store the max value from houses 2 away or more
         for(int i = 2; i < size; i++){
+            //stores best value 2 houses away at least
             temp = max(a, temp);
+            //a is valuse of prev house
             a = b;
+            //choose either best value 2 away + current value of value calculated for prev
             b = max(nums[i] + temp,b);
         }
         return max(a, b);
