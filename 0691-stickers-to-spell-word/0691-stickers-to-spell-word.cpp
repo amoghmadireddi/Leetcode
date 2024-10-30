3ec7fa69be98b101e13b6723dcd9b70d7f2f3a3f
Amogh Madireddi <amoghmm@umich.edu>
2024-10-30 13:16:21 -0400

    Time: 28 ms (91.96%), Space: 17 MB (66.44%) - LeetHub

--- 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
@@ -12,20 +12,31 @@ public:
     }
 private:
     int helper(unordered_map<string, int>& dp, vector<vector<int>>& mp, string target) {
-        if (dp.count(target)) return dp[target];
-        int ans = INT_MAX, n = mp.size();
+        if (dp.count(target)){
+            return dp[target];
+        }
+        int ans = INT_MAX;
+        int n = mp.size();
         vector<int> tar(26, 0);
-        for (char c:target) tar[c-'a']++;
+        for (char c:target){
+            tar[c-'a']++;
+        }
         // try every sticker
         for (int i = 0; i < n; i++) {
             // optimization
-            if (mp[i][target[0]-'a'] == 0) continue; 
+            if (mp[i][target[0]-'a'] == 0){
+                continue; 
+            }
             string s;
             // apply a sticker on every character a-z
             for (int j = 0; j < 26; j++) 
-                if (tar[j]-mp[i][j] > 0) s += string(tar[j]-mp[i][j], 'a'+j);
+                if (tar[j]-mp[i][j] > 0){
+                    s += string(tar[j]-mp[i][j], 'a'+j);
+                }
             int tmp = helper(dp, mp, s);
-            if (tmp!= -1) ans = min(ans, 1+tmp);
+            if (tmp!= -1){
+                 ans = min(ans, 1+tmp);
+            }
         }
         dp[target] = ans == INT_MAX? -1:ans;
         return dp[target];
