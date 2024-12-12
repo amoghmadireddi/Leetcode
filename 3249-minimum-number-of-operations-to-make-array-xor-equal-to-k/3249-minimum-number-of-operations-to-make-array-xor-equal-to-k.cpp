11b794c7b9e604c4f439eb0ae69e031bdbfd488d
Amogh Madireddi <amoghmm@umich.edu>
2024-12-12 00:30:42 -0500

    Time: 104 ms (63.52%), Space: 92.8 MB (18.18%) - LeetHub

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -3,7 +3,7 @@ public:
     int minOperations(vector<int>& nums, int k) {
         int n = nums.size();
         for(int i = 0; i < n; i++){
-            k = k ^ nums[i];
+            k ^= nums[i];
         }
         int count = 0;
         while (k) {
